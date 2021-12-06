a7674ec5ef0e3dd5842b46b79420bb365eef941d
Fenhl <fenhl@fenhl.net>
2021-12-06 14:13:41 +0000

    Template for daily module

---
+++ src/template.rs
@@ -0,0 +1,18 @@
+use crate::prelude::*;
+
+#[aoc_generator(day)]
+fn gen(input: &str) -> ! {
+    unimplemented!()
+}
+
+#[aoc(day, part1)]
+fn part1(input: &!) -> ! {
+    unimplemented!()
+}
+
+/*
+#[aoc(day, part2)]
+fn part2(input: &!) -> ! {
+    unimplemented!()
+}
+*/
